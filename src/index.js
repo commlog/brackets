3ae18b88c3032e395bcc88cd8225b035c4e6ace8
commlog <106234155+commlog@users.noreply.github.com>
2023-01-26 15:01:20 +0300

    Update index.js
    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  function check(str, bracketsConfig) {
+    var stack = [];
+    for (var i = 0; i < str.length; i++) {
+        var currentBracket = str[i];
+        for (var j = 0; j < bracketsConfig.length; j++) {
+            var openBracket = bracketsConfig[j][0];
+            var closeBracket = bracketsConfig[j][1];
+            if (currentBracket === openBracket) {
+                stack.push(currentBracket);
+                break;
+            }
+            else if (currentBracket === closeBracket) {
+                if (stack.length === 0 || stack[stack.length - 1] !== openBracket) {
+                    return false;
+                }
+                else {
+                    stack.pop();
+                    break;
+                }
+            }
+        }
+    }
+    return stack.length === 0;
 }
